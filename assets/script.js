d3da6291c8079d6220468911fbca65e0087f8ee9
nverhulp <105401594+nverhulp@users.noreply.github.com>
2022-08-01 22:42:05 -0700

    time tracker

--- assets/script.js
+++ assets/script.js
@@ -9,4 +9,30 @@ $(document).ready(functio()) {
         var time = $(this).parent().attr("id");
         localStorage.setItem(time, text);
     }
+
+    function timeTracker() {
+        var timeNow = moment().hour();
+
+        $(".time-blcok").each(function()) {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        }
+    }
 }
